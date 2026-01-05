ea51cb8bc9f6bc5a40c0783c08bd226838a8e50f
WaffleWrap <reda.naji@insa-lyon.fr>
2026-01-05 15:30:12 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"math/rand"
 	"time"
 )
 
 func main() {
+	// Initialisation de l'aléatoire
 	rand.Seed(time.Now().UnixNano())
 
-	// Gestion des arguments ligne de commande
-	mode := flag.String("mode", "server", "Mode de lancement: 'server' ou 'client'")
+	// Lecture des arguments (ex: -mode=client)
+	mode := flag.String("mode", "server", "Mode: 'server' ou 'client'")
 	addr := flag.String("addr", "localhost:8080", "Adresse du serveur (pour le client)")
 	
 	flag.Parse()
 
 	if *mode == "server" {
-		LaunchServer()
+		LaunchServer() // Défini dans server.go
+	} else if *mode == "client" {
+		LaunchClient(*addr) // Défini dans client.go
 	} else {
-		LaunchClient(*addr)
+		fmt.Println("Mode inconnu. Utilisez -mode=server ou -mode=client")
 	}
 }
